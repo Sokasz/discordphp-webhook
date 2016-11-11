238b6b99413f744e03e1fd78d2209573362da827
Joshua Cleland <proxikal@gmail.com>
2016-11-10 22:59:26 -0500

    Updating some things.

--- discordphp-webhook.php
+++ discordphp-webhook.php
@@ -1,19 +1,28 @@
 <?php
-
 	Class xTechHooks {
 		/* Main variables */
 		public $HookUrl = '';
 		public $BotName = '';
 		public $Message = '';
+		public $TTS = false;
 		public $Avatar = '';
+		public $ThumbUrl = '';
+		public $ThumbHeight = '';
+		public $ThumbWidth = '';
+
 
 		public function DiscordPost($embeds = null) {
 			$user["username"] = $this->BotName;
 			$user["avatar_url"] = $this->Avatar;
 			$user["content"] = $this->Message;
+			$user["tts"] = $this->TTS;
 			$hookURL = $this->HookUrl;
 			if(isset($embeds)) {
 				$user["embeds"] = $embeds;
+			}
+			if(!isset($this->HookUrl)) {
+				echo "<b><font color='red'>Error: Missing the HookUurl variable or null.";
+				exit(0);
 			}
 		    $curl = curl_init($hookURL);
 		    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
@@ -25,13 +34,27 @@
 			$var = array();
 			$obj = array("url" => $url, "height" => $height, "width" => $width);
 		    $e = array("image" => $obj);
-		    $var[$num] = $e;
 		    return $var[$num] = $e;
 		}
-		public function PushLink($url = null, $title = null, $desc = null, $thumb = null) {
-			if(!isset($thumb)) {
+		public function PushAuthor($num = 0, $name = null, $url = null, $icon_url = null) {
+			$var = array();
+			$obj = array("name" => $name, "url" => $url, "icon_url" => $icon_url);
+		    $e = array("author" => $obj);
+		    return $var[$num] = $e;
+		}
+		public function PushFooter($num = 0, $text = null, $icon_url = null) {
+			$var = array();
+			$obj = array("text" => $text, "icon_url" => $icon_url);
+		    $e = array("footer" => $obj);
+		    return $var[$num] = $e;
+		}
+		public function PushLink($url = null, $title = null, $desc = null) {
+			if(!isset($this->ThumbUrl)) {
 				$obj = array("url" => $url, "title" => $title, "description" => $desc);
 			} else {
+				$thumb["url"] = $this->ThumbUrl;
+				$thumb["height"] = ($this->ThumbHeight != null ? $this->ThumbHeight : 64);
+				$thumb["width"] = ($this->ThumbWidth != null ? $this->ThumbWidth : 64);
 				$obj = array("url" => $url, "title" => $title, "description" => $desc, "thumbnail" => $thumb);
 			}
 		    return $obj;
